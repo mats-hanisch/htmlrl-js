1d833731f931963dcbc6c8688a4f3d4e28764934
matshx <mats.hanisch@gmail.com>
2026-02-16 20:44:22 +0100

    Add comments

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,5 +1,8 @@
 import { writeFileSync } from "node:fs";
 
+
+// create a package.json in the CJS build to override the root "type": "module"
+// this ensures Node treats the CJS files as CommonJS
 writeFileSync(
   "dist/cjs/package.json",
   JSON.stringify({ type: "commonjs" }, null, 2)
